719da8f5102e636640c4740dad81074ebe738a38
Raoul Duke <raould@gmail.com>
2025-10-26 21:05:48 -0700

    dockering checkpoint

--- Docker/docker-build.sh
+++ Docker/docker-build.sh
@@ -13,3 +13,9 @@ docker build -t ${DAEMON} -f Docker/Dockerfile.pybakd .
 CLIENT=pybak-client
 docker rm -f ${CLIENT}
 docker build -t ${CLIENT} -f Docker/Dockerfile.client .
+
+docker container prune -f
+docker image prune -f
+docker image list
+
+
--- Docker/docker-run-pybakd.sh
+++ Docker/docker-run-pybakd.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+echo "pruning..."
 IMAGE_NAME=pybakd
 docker ps --filter "ancestor=${IMAGE_NAME}" --format "{{.Names}}" | xargs -I X docker container stop "X"
-docker container prune
+docker container prune -f
+echo "...done pruning"
 
+echo "starting..."
 NETWORK=pybak-network
 docker network create ${NETWORK}
 docker run \
        --name pybakd1 \
-       --network ${NETWORK}
-       -d \
+       --network ${NETWORK} \
+       -t \
        -v /home/pybak/canonical:/home/pybak/canonical \
        -v /home/pybak/browse:/home/pybak/browse \
        -p 6969:6969 \
+       --detach \
        pybakd
+echo "...started"
